3935abf4ce870a3823d197cd5a7224bf1dd89641
Bryan <junhang.lee@ciceron.me>
2018-04-30 04:03:15 +0900

    이제 메일도 자동

--- app/subscribe/model.py
+++ app/subscribe/model.py
@@ -1,5 +1,9 @@
 from flask import g
 import traceback
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 def addList(email):
     cursor = g.db.cursor()
@@ -15,4 +19,21 @@ def addList(email):
         return False
 
     g.db.commit()
+
+    msg = MIMEMultipart()
+
+    msg['From'] = 'contact@langchain.io'
+    msg['Subject'] = "Thank you for subscribing LangChain! (former, Mother of Language)"
+    msg['To'] = email
+    with open('templates/letter.txt', 'r') as f:
+        text = f.read()
+    content = MIMEText(text, 'plain', _charset='utf-8')
+    msg.attach(content)
+
+    a = smtplib.SMTP('smtp.gmail.com:587')
+    a.ehlo()  
+    a.starttls()
+    a.login('contact@langchain.io', 'ciceron3388!')
+    a.sendmail(msg['From'], msg['To'], msg.as_string())
+
     return True
