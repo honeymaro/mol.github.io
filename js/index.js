524b36294e061bcacde94d7b9a4e64ac0fa17eb7
Kim Hanseulmaro <honeymaro@live.co.kr>
2018-05-11 23:23:14 +0900

    LangChain 버전으로 제작한다
    - 로고 변경,
    - 스크롤 가능하게 변경
    - 동영상 삽입
    - 텔레그램 삽입

--- js/index.js
+++ js/index.js
@@ -6,15 +6,27 @@ var animation = bodymovin.loadAnimation({
 	path: 'logo.json'
 });
 
-var domAnimationList = document.getElementsByClassName("animation");
-var i = 0;
-var fnAnimation = function (el, ms) {
-	setTimeout(function () {
-		el.classList.remove("animation");
-	}, ms);
-}
+var fnWindowResize = function () {
 
-for (i = 0; i < domAnimationList.length; i++) {
-	var el = domAnimationList.item(i);
-	fnAnimation(el, i * 250);
+	var domPadding = document.getElementsByClassName("content-padding").item(0);
+	var domLogo = document.getElementById("logo");
+
+	domPadding.style.height = (parseFloat(getComputedStyle(domLogo)["top"]) - (parseFloat(getComputedStyle(domLogo)["height"]) / 2.0)) + "px";
+
 }
+animation.addEventListener('DOMLoaded', function () {
+	fnWindowResize();
+	window.addEventListener('resize', fnWindowResize, true);
+	var domAnimationList = document.getElementsByClassName("animation");
+	var i = 0;
+	var fnAnimation = function (el, ms) {
+		setTimeout(function () {
+			el.classList.remove("animation");
+		}, ms);
+	}
+
+	for (i = 0; i < domAnimationList.length; i++) {
+		var el = domAnimationList.item(i);
+		fnAnimation(el, i * 250);
+	}
+});
